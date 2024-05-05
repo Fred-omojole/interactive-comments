7f56b39f2307b87dc32605431726c2f0a2f47ba4
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-05-05 08:50:30 +0100

    issues

--- app/(home)/components/Comments.tsx
+++ app/(home)/components/Comments.tsx
@@ -3,33 +3,42 @@ import React, { useState } from "react";
 import { Data, Comment, Reply } from "@/lib/Definition";
 const Comments = () => {
   const [comment, setComment] = useState(Data);
+  const [commentBody, setCommentBody] = useState("");
+
   return (
     <>
       <div className="space-x-2">
         <label>
           <input
-            // value={comment}
-            // onChange={(e) => setComment (e.target.value)}
+            value={commentBody}
+            onChange={(e) => setCommentBody(e.target.value)}
             placeholder="Enter Message ..."
             className="border-[1px] border-slate-900 p-1/2 rounded-md p-2"
           />
         </label>
 
-        <button className="border-[1px] rounded-md border-slate-900 px-1">
+        <button
+          // onClick={}
+          className="border-[1px] rounded-md border-slate-900 px-1"
+        >
           Send
         </button>
       </div>
 
-      <div>
-        {Data.map((root, rootIndex) => (
-          <div key={rootIndex}>
-            <div>
+      <div className=" mt-10">
+        {comment.map((root, rootIndex) => (
+          <div className="" key={rootIndex}>
+            <div className="flex flex-col gap-4">
               {root.comments.map((comment: Comment, commentIndex) => (
-                <div key={commentIndex}>
-                  <div>{comment.content}</div>
+                <div className="flex flex-col gap-4" key={commentIndex}>
+                  <div className=" border-[1px] border-zinc-500 rounded-md">
+                    {comment.content}
+                  </div>
                   {comment.replies.map((reply: Reply, replyIndex) => (
                     <div key={replyIndex}>
-                      <div>{reply.content}</div>
+                      <div className=" border-[1px] border-zinc-500 rounded-md">
+                        {reply.content}
+                      </div>
                     </div>
                   ))}
                 </div>
