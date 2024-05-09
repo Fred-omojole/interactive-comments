f5189742d63c63847a0297329fdf4e93d9577b3f
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-05-09 10:52:17 +0100

    styling

--- app/(home)/components/Comments.tsx
+++ app/(home)/components/Comments.tsx
@@ -29,47 +29,46 @@ const Comments = () => {
 
   return (
     <div className="">
-     
-     <div className="p-3 mt-10 bg-[hsl(228,33%,97%)] rounded-md lg:flex lg:gap-2 lg:w-3/4 lg:p-4">
-     <div className=" lg:h-36">
-        <Image
-        className="lg:hidden mb-4 lg:m-auto"
-          src={Data.currentUser.image.png}
-          alt="current user "
-          width={50}
-          height={50}
-        />
+      <div className="p-3 mt-10 bg-[hsl(228,33%,97%)] rounded-md lg:flex lg:gap-2 lg:w-3/4 lg:p-4">
+        <div className=" lg:h-36">
+          <Image
+            className="lg:hidden mb-4 lg:m-auto"
+            src={Data.currentUser.image.png}
+            alt="current user "
+            width={50}
+            height={50}
+          />
 
-<Image
-        className="hidden lg:block"
-          src={Data.currentUser.image.png}
-          alt="current user "
-          width={80}
-          height={60}
-        />
+          <Image
+            className="hidden lg:block"
+            src={Data.currentUser.image.png}
+            alt="current user "
+            width={80}
+            height={60}
+          />
+        </div>
 
+        <label className="w-[1024px] ">
+          {" "}
+          <textarea
+            className="w-full p-6 mb-2 h-full bg-[hsl(228,33%,97%)]"
+            placeholder="Add Comments..."
+            value={commentBody}
+            onChange={(e) => setCommentBody(e.target.value)}
+          ></textarea>
+        </label>
 
+        <div className="p-2 grid place-content-end lg:flex lg:items-start mt-2 mb-4 ">
+          <button
+            onClick={() => {
+              handleComment();
+            }}
+            className="relative capitalize font-semibold text-white rounded-md moderateBlue  px-6 py-2 lg:px-8 lg:py-2 cursor-pointer  "
+          >
+            send
+          </button>
+        </div>
       </div>
-
-      <label className="w-[1024px] ">
-        {" "}
-        <textarea
-          className="w-full p-6 mb-2 h-full bg-[hsl(228,33%,97%)]"
-          placeholder="Add Comments..."
-          value={commentBody}
-          onChange={(e) => setCommentBody(e.target.value)}
-        ></textarea>
-      </label>
-
-      <div className="p-2 grid place-content-end lg:flex lg:items-start mt-2 mb-4 ">
-        <button onClick={()=> {handleComment()}} className="relative capitalize font-semibold text-white rounded-md moderateBlue  px-6 py-2 lg:px-4 lg:py-2   ">
-          send
-        </button>
-      </div>
-
-
-     </div>
-
     </div>
   );
 };
